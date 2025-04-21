15bdb03a1f889841bbe66ad1caee3696b857ee7e
M3hTi <mehdinasir502@yahoo.com>
2025-04-21 17:31:08 +0330

    feat: ✨ Add Spinner component for loading indication using LDRS Ring

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import { Ring } from "ldrs/react";
+import "ldrs/react/Ring.css";
+
+function Spinner() {
+  return <Ring size="40" stroke="5" bgOpacity="0" speed="2" color="white" />;
+}
+
+export default Spinner;
