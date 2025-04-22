0a698cb25df4ed32c3321e5af63396df190df408
M3hTi <mehdinasir502@yahoo.com>
2025-04-22 15:53:45 +0330

    feat: ✨ Implement movie preview functionality and add blur effect to movie grid

--- src/features/movies/MovieList.jsx
+++ src/features/movies/MovieList.jsx
@@ -1,13 +1,25 @@
 import { useSelector } from "react-redux";
 import Movie from "./Movie";
-import styles from './MovieList.module.css'
+import styles from "./MovieList.module.css";
+import { useState } from "react";
+import Preview from "./Preview";
+
 function MovieList() {
+  const [isMovieOpen, setIsMovieOpen] = useState(false);
   const movies = useSelector((store) => store.moviesState.movies);
+
+  function handleClose() {
+    setIsMovieOpen(false);
+  }
+
   return (
-    <div className={styles.movieGrid}>
-      {movies.map((movie) => (
-        <Movie key={movie.id} movieObj={movie} />
-      ))}
+    <div className={styles.container}>
+      <div className={isMovieOpen ? styles.blur : styles.movieGrid}>
+        {movies.map((movie) => (
+          <Movie key={movie.id} movieObj={movie} setOpen={setIsMovieOpen} />
+        ))}
+      </div>
+      {isMovieOpen && <Preview onClose={handleClose} />}
     </div>
   );
 }
