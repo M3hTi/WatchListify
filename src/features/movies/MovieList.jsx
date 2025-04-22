97620411386452efb97826ce3fd9f2287ee74e42
M3hTi <mehdinasir502@yahoo.com>
2025-04-22 16:32:46 +0330

    feat: ✨ Integrate loading spinner in MovieList component while fetching movie data

--- src/features/movies/MovieList.jsx
+++ src/features/movies/MovieList.jsx
@@ -3,10 +3,12 @@ import Movie from "./Movie";
 import styles from "./MovieList.module.css";
 import { useState } from "react";
 import Preview from "./Preview";
+import { Show } from "react-smart-conditional";
+import Spinner from "../../components/Spinner";
 
 function MovieList() {
   const [isMovieOpen, setIsMovieOpen] = useState(false);
-  const movies = useSelector((store) => store.moviesState.movies);
+  const { movies, isLoading } = useSelector((store) => store.moviesState);
 
   function handleClose() {
     setIsMovieOpen(false);
@@ -19,7 +21,18 @@ function MovieList() {
           <Movie key={movie.id} movieObj={movie} setOpen={setIsMovieOpen} />
         ))}
       </div>
-      {isMovieOpen && <Preview onClose={handleClose} />}
+      {isMovieOpen && (
+        <Show>
+          <Show.If condition={isLoading}>
+            <div className={styles.loading}>
+              <Spinner color="#e50914" />
+            </div>
+          </Show.If>
+          <Show.Else>
+            <Preview onClose={handleClose} />
+          </Show.Else>
+        </Show>
+      )}
     </div>
   );
 }
