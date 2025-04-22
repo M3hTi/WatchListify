2eeb6d97a8655d644419571c8ddfcd7cf2841de9
M3hTi <mehdinasir502@yahoo.com>
2025-04-22 15:54:32 +0330

    feat: ✨ Add movie detail fetching functionality and update loading state management

--- src/features/movies/MovieSlice.jsx
+++ src/features/movies/MovieSlice.jsx
@@ -1,16 +1,20 @@
 const key = import.meta.env.VITE_API_KEY;
 console.log(key);
+const omdbApi = import.meta.env.VITE_OMDB_API_KEY;
 const initialMovieState = {
   movies: [],
   isLoading: false,
+  selectedMovie: null,
 };
 
 function movieReducer(state = initialMovieState, action) {
   switch (action.type) {
     case "movie/loading":
-      return { ...state, isLoading: true };
+      return { ...state, isLoading: true, selectedMovie: null };
     case "movie/dataReceived":
       return { ...state, movies: action.payload, isLoading: false };
+    case "movie/getDetail":
+      return { ...state, selectedMovie: action.payload, isLoading: false };
     default:
       return state;
   }
@@ -30,6 +34,21 @@ export function dataReceived() {
     } catch (error) {
       console.log(error);
     }
+  };
+}
+
+export function getDetail(movieObj) {
+  return async function movieDetail(dispatch, getState) {
+    const { title, release_date: date } = movieObj;
+    dispatch({ type: "movie/loading" });
+    const res = await fetch(
+      `http://www.omdbapi.com/?apikey=${omdbApi}&t=${title}&y=${
+        date.split("-")[0]
+      }&type=movie`
+    );
+    const data = await res.json();
+    // console.log(data);
+    dispatch({ type: "movie/getDetail", payload: data });
   };
 }
 
