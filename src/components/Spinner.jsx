75715912469c46d9853208e1880ebdbb5c7e1f09
M3hTi <mehdinasir502@yahoo.com>
2025-04-22 16:31:48 +0330

    feat: ✨ Update Spinner component to accept customizable color prop

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,8 +1,8 @@
 import { Ring } from "ldrs/react";
 import "ldrs/react/Ring.css";
 
-function Spinner() {
-  return <Ring size="40" stroke="5" bgOpacity="0" speed="2" color="white" />;
+function Spinner({ color = "white" }) {
+  return <Ring size="40" stroke="5" bgOpacity="0" speed="2" color={color} />;
 }
 
 export default Spinner;
