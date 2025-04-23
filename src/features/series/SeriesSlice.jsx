f51d4fee35bf44272c42db11db37267ea7cc32a7
M3hTi <mehdinasir502@yahoo.com>
2025-04-23 15:11:41 +0330

    feat: ✨ Add async action to fetch individual series details and update state management in SeriesSlice

--- src/features/series/SeriesSlice.jsx
+++ src/features/series/SeriesSlice.jsx
@@ -1,16 +1,20 @@
 const tmdbnKey = import.meta.env.VITE_API_KEY;
+const omdbApi = import.meta.env.VITE_OMDB_API_KEY;
 
 const initialStateSeries = {
   series: [],
   isLoading: false,
+  selectedSerie: null,
 };
 
 function seriesReducer(state = initialStateSeries, action) {
   switch (action.type) {
     case "series/loading":
-      return { ...state, isLoading: true };
+      return { ...state, isLoading: true, selectedSerie: null };
     case "series/dataReceived":
       return { ...state, isLoading: false, series: action.payload };
+    case "series/getSerie":
+      return { ...state, selectedSerie: action.payload, isLoading: false };
     default:
       return state;
   }
@@ -24,6 +28,20 @@ export function dataReceived() {
     );
     const data = await res.json();
     dispatch({ type: "series/dataReceived", payload: data.results });
+  };
+}
+
+export function getSerie(serieObj) {
+  return async function getDatil(dispatch, getState) {
+    const { name: title, first_air_date: date } = serieObj;
+    dispatch({ type: "series/loading" });
+    const res = await fetch(
+      `http://www.omdbapi.com/?apikey=${omdbApi}&t=${title}&y=${
+        date.split("-")[0]
+      }&type=series`
+    );
+    const data = await res.json();
+    dispatch({ type: "series/getSerie", payload: data });
   };
 }
 
