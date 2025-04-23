19c54537b2bb29d9fc700c5cc538092668cd59c7
M3hTi <mehdinasir502@yahoo.com>
2025-04-23 15:12:33 +0330

    feat: ✨ Enhance SeriesList component with loading state and preview functionality for individual series

--- src/features/series/SeriesList.jsx
+++ src/features/series/SeriesList.jsx
@@ -1,17 +1,34 @@
 import { useSelector } from "react-redux";
 import Serie from "./Serie";
-import styles from './SeriesList.module.css'
-
+import styles from "./SeriesList.module.css";
+import { useState } from "react";
+import { Show } from "react-smart-conditional";
+import Spinner from "../../components/Spinner";
+import Preview from "./Preview";
 
 function SeriesList() {
-  const series = useSelector((store) => store.seriesState.series);
+  const [isSerieOpen, setIsSerieOpen] = useState(false);
+  const { series, isLoading } = useSelector((store) => store.seriesState);
+
   return (
     <div className={styles.container}>
-      <div className={styles.seriesGrid}>
+      <div className={isSerieOpen ? styles.blur : styles.seriesGrid}>
         {series.map((serie) => (
-          <Serie serieObj={serie} key={serie.id} />
+          <Serie serieObj={serie} key={serie.id} setOpen={setIsSerieOpen} />
         ))}
       </div>
+      {isSerieOpen && (
+        <Show>
+          <Show.If condition={isLoading}>
+            <div className={styles.loading}>
+              <Spinner color="#e50914" />
+            </div>
+          </Show.If>
+          <Show.Else>
+            <Preview setOpen={setIsSerieOpen} />
+          </Show.Else>
+        </Show>
+      )}
     </div>
   );
 }
