057f90f46f581de4a2af0cef1d6a226687f7fcb7
M3hTi <mehdinasir502@yahoo.com>
2025-04-23 15:16:15 +0330

    feat: ✨ Add search functionality to Movies component with query state management

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,26 +1,31 @@
 import styles from "./Movies.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { dataReceived } from "../features/movies/MovieSlice";
+import { useEffect, useState } from "react";
+import { dataReceived, searchMovie } from "../features/movies/MovieSlice";
 import Spinner from "../components/Spinner";
 import { Show } from "react-smart-conditional";
 import MovieList from "../features/movies/MovieList";
 import Header from "../components/Header";
 
 function Movies() {
-  const { isLoading } = useSelector((store) => store.moviesState.isLoading);
+  const [query, setQuery] = useState("");
+  const { isLoading, movies } = useSelector((store) => store.moviesState);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(dataReceived());
   }, [dispatch]);
 
+  useEffect(() => {
+    dispatch(searchMovie(query));
+  }, [dispatch, query]);
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header query={query} setQuery={setQuery} />
       <main className={styles.main}>
         <Show>
-          <Show.If condition={isLoading}>
+          <Show.If condition={isLoading && movies.length === 0}>
             <div className={styles.loading}>
               <Spinner />
             </div>
