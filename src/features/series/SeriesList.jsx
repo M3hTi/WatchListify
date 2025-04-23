ce89ca7af33a61583a877a40d93fdb4cad9d076a
M3hTi <mehdinasir502@yahoo.com>
2025-04-23 11:59:30 +0330

    feat: ✨ Enhance SeriesList component to display series from Redux store and add CSS styles

--- src/features/series/SeriesList.jsx
+++ src/features/series/SeriesList.jsx
@@ -1,5 +1,19 @@
+import { useSelector } from "react-redux";
+import Serie from "./Serie";
+import styles from './SeriesList.module.css'
+
+
 function SeriesList() {
-  return <div>This is a List!</div>;
+  const series = useSelector((store) => store.seriesState.series);
+  return (
+    <div>
+      <div>
+        {series.map((serie) => (
+          <Serie serieObj={serie} key={serie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SeriesList;
