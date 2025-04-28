0929fbf6bdf932fa68c18f134e1c70548306e629
M3hTi <mehdinasir502@yahoo.com>
2025-04-28 15:39:16 +0330

    feat: ✨ Enhance MovieList component with loading and error handling, and improve no results display

--- src/features/movies/MovieList.jsx
+++ src/features/movies/MovieList.jsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { useSelector } from "react-redux";
 import Movie from "./Movie";
 import styles from "./MovieList.module.css";
@@ -8,7 +9,9 @@ import Spinner from "../../components/Spinner";
 
 function MovieList() {
   const [isMovieOpen, setIsMovieOpen] = useState(false);
-  const { movies, isLoading } = useSelector((store) => store.moviesState);
+  const { movies, isLoading, error } = useSelector(
+    (store) => store.moviesState
+  );
 
   function handleClose() {
     setIsMovieOpen(false);
@@ -17,9 +20,32 @@ function MovieList() {
   return (
     <div className={styles.container}>
       <div className={isMovieOpen ? styles.blur : styles.movieGrid}>
-        {movies.map((movie) => (
-          <Movie key={movie.id} movieObj={movie} setOpen={setIsMovieOpen} />
-        ))}
+        <Show as={React.Fragment}>
+          <Show.If condition={isLoading}>
+            <div className={styles.loading}>
+              <Spinner color="#e50914" />
+            </div>
+          </Show.If>
+          <Show.If condition={error} as={React.Fragment}>
+            <div className={styles.error}>
+              <p>{error}</p>
+            </div>
+          </Show.If>
+          <Show.If condition={movies.length > 0} as={React.Fragment}>
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id || movie.imdbID}
+                movieObj={movie}
+                setOpen={setIsMovieOpen}
+              />
+            ))}
+          </Show.If>
+          <Show.Else as={React.Fragment}>
+            <div className={styles.noResults}>
+              <p>No movies found. Try a different search term.</p>
+            </div>
+          </Show.Else>
+        </Show>
       </div>
       {isMovieOpen && (
         <Show>
