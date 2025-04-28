8e01ce639fdd4aed6bfb26a14345057241644f31
M3hTi <mehdinasir502@yahoo.com>
2025-04-28 15:46:25 +0330

    feat: ✨ Implement search functionality in SeriesSlice with error handling and loading state management

--- src/features/series/SeriesSlice.jsx
+++ src/features/series/SeriesSlice.jsx
@@ -5,16 +5,32 @@ const initialStateSeries = {
   series: [],
   isLoading: false,
   selectedSerie: null,
+  error: "",
 };
 
 function seriesReducer(state = initialStateSeries, action) {
   switch (action.type) {
     case "series/loading":
-      return { ...state, isLoading: true, selectedSerie: null };
+      return { ...state, isLoading: true, selectedSerie: null, error: "" };
     case "series/dataReceived":
       return { ...state, isLoading: false, series: action.payload };
     case "series/getSerie":
-      return { ...state, selectedSerie: action.payload, isLoading: false };
+      return {
+        ...state,
+        selectedSerie: action.payload,
+        isLoading: false,
+        error: "",
+      };
+    case "series/searchSeries":
+      return { ...state, series: action.payload, isLoading: false };
+    case "series/error":
+      return {
+        ...state,
+        series: [],
+        isLoading: false,
+        selectedSerie: null,
+        error: action.payload,
+      };
     default:
       return state;
   }
@@ -42,6 +58,38 @@ export function getSerie(serieObj) {
     );
     const data = await res.json();
     dispatch({ type: "series/getSerie", payload: data });
+  };
+}
+
+export function searchSeries(query, signal) {
+  return async function serach(dispatch, getState) {
+    try {
+      dispatch({ type: "series/loading" });
+      if (!query) {
+        dispatch(dataReceived());
+        return;
+      }
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${omdbApi}&s=${query}&type=series`,
+        { signal }
+      );
+      if (!res.ok) throw new Error("Error occurred while fetching data");
+
+      const data = await res.json();
+
+      // console.log(data);
+
+      if (data.Response === "True") {
+        console.log(data.Search);
+        dispatch({ type: "series/searchSeries", payload: data.Search });
+      } else {
+        console.log(data.Error);
+        dispatch({ type: "series/error", payload: data.Error });
+      }
+    } catch (error) {
+      console.log(error.message);
+      dispatch({ type: "series/error", payload: error.message });
+    }
   };
 }
 
